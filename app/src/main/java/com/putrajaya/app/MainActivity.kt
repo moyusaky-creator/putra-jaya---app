aa48b0a6d316e7be9bf28e98f88fe98ef357826d
moyusaky-creator <moyusaky@gmail.com>
2026-08-25 12:38:59 +0700

    Add Android WebView launcher

---
+++ app/src/main/java/com/putrajaya/app/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.putrajaya.app
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+    private lateinit var web: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        web = WebView(this)
+        web.settings.javaScriptEnabled = true
+        web.settings.domStorageEnabled = true
+        web.settings.databaseEnabled = true
+        web.settings.allowFileAccess = true
+        web.settings.allowContentAccess = true
+        web.settings.cacheMode = WebSettings.LOAD_DEFAULT
+        web.webChromeClient = WebChromeClient()
+        web.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean = false
+        }
+        setContentView(web)
+        web.loadUrl("https://moyusaky-creator.github.io/putra-jaya---app/PUTRAJAYA_2_ONLINE.html")
+    }
+    override fun onBackPressed() { if (web.canGoBack()) web.goBack() else super.onBackPressed() }
+}
