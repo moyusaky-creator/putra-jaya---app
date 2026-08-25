58e6fe5260530f3903b4dda47b50445df5db3e4c
moyusaky-creator <moyusaky@gmail.com>
2026-08-25 12:22:36 +0700

    Refactor MainActivity for enhanced WebView functionality
    Refactored MainActivity to improve WebView handling and added support for content access and zoom controls.

--- android/app/src/main/java/com/putrajaya/app/MainActivity.java
+++ android/app/src/main/java/com/putrajaya/app/MainActivity.java
@@ -5,13 +5,69 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.WebResourceRequest;
+import android.graphics.Bitmap;
+import android.view.View;
 
 public class MainActivity extends Activity {
-  @Override public void onCreate(Bundle b){ super.onCreate(b);
-    WebView w=new WebView(this); setContentView(w);
-    WebSettings s=w.getSettings(); s.setJavaScriptEnabled(true); s.setDomStorageEnabled(true); s.setDatabaseEnabled(true); s.setAllowFileAccess(true); s.setSupportZoom(false);
-    w.setWebViewClient(new WebViewClient());
-    w.loadUrl("https://moyusaky-creator.github.io/putra-jaya---app/PUTRAJAYA_2_ONLINE.html");
-  }
-  @Override public void onBackPressed(){ WebView w=(WebView)findViewById(android.R.id.content); super.onBackPressed(); }
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setSupportZoom(false);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(
+                    WebView view,
+                    WebResourceRequest request) {
+                return false;
+            }
+
+            @Override
+            public void onPageStarted(
+                    WebView view,
+                    String url,
+                    Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+        });
+
+        webView.loadUrl(
+            "https://moyusaky-creator.github.io/putra-jaya---app/PUTRAJAYA_2_ONLINE.html"
+        );
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.destroy();
+        }
+        super.onDestroy();
+    }
 }
